4ad1ab678c344891848ee9987d7aaa5bb80b6789
maksimsemenov <maksa.semenov@gmail.com>
2018-04-13 10:11:32 -0400

    Add responsivness and fix support for various range values

--- src/slidery.js
+++ src/slidery.js
@@ -31,7 +31,17 @@ const formatValue = (value, precision = 0, separator = '.') => {
   }
   return formattedValue
 }
-
+const debounce = (func, wait = 100) => {
+  let timer
+  return (...args) => {
+    if (timer) {
+      clearTimeout(timer)
+    }
+    timer = setTimeout(() => {
+      func(...args)
+    }, wait)
+  }
+}
 const funcIdRegExp = /\[[\w-_\.]+\]/gi
 const calculateValue = (func, sliders) => {
   let funcWithValues = func.match(funcIdRegExp).reduce((tempFunc, match) => {
@@ -40,10 +50,7 @@ const calculateValue = (func, sliders) => {
     if (!slider) {
       return tempFunc
     }
-    const value =
-      slider.tempValue !== null && slider.tempValue !== undefined
-        ? slider.tempValue
-        : slider.value
+    const value = slider.value
     return tempFunc.replace(match, value)
   }, func)
   funcWithValues = funcWithValues.replace(',', '.')
@@ -84,13 +91,24 @@ class Slidery {
           (value && value.indexOf(id) !== -1) || (!value && !node.id)
       )
       const range =
-        node.dataset.range && /\d+\.\.\.\d+/.test(node.dataset.range)
-          ? node.dataset.range.split('...')
+        node.dataset.range && /-?\d+\.\.\.-?\d+/.test(node.dataset.range)
+          ? node.dataset.range.split('...').map(i => parseInt(i, 10))
           : [0, width]
       const scale = width / (range[1] - range[0])
-      let initialValue = parseInt(node.dataset.value, 10) || width / 2
-      if (initialValue < range[0]) initialValue = range[0]
-      if (initialValue > range[1]) initialValue = range[1]
+
+      let initialValue, initialProgress
+      if (
+        node.dataset.initialValue &&
+        parseInt(node.dataset.initialValue, 10)
+      ) {
+        initialValue = parseInt(node.dataset.initialValue, 10)
+        if (initialValue < range[0]) initialValue = range[0]
+        if (initialValue > range[1]) initialValue = range[1]
+        initialProgress = (initialValue - range[0]) * scale
+      } else {
+        initialProgress = width / 2
+        initialValue = range[0] + initialProgress / scale
+      }
 
       const listeners = {
         baseClick: this.handleBaseNodeClick(id),
@@ -104,24 +122,35 @@ class Slidery {
       nodes.liftNode.addEventListener('mousedown', listeners.liftMouseDown)
       nodes.liftNode.addEventListener('touchstart', listeners.touchStart)
 
-      const liftPosition = scale * initialValue
-      nodes.liftNode.style.left = `${liftPosition}px`
-      nodes.progressNode.style.width = `${liftPosition}px`
+      const liftPosition = initialProgress / width * 100
+      nodes.liftNode.style.left = `${liftPosition}%`
+      nodes.progressNode.style.width = `${liftPosition}%`
 
       this.sliders[id] = {
         id,
         nodes,
         targets,
         width,
-        initialValue,
         range,
         scale,
-        value: initialValue * scale
+        value: initialValue,
+        progress: initialProgress
       }
     })
 
     Object.values(this.sliders).forEach(slider => {
       this.adjustTargets(slider.targets, slider.value)
+    })
+    window.addEventListener('resize', debounce(this.handleWindowResize, 300))
+  }
+
+  // Window handlers
+  handleWindowResize = () => {
+    Object.values(this.sliders).forEach(slider => {
+      const newWidth = slider.nodes.baseNode.getBoundingClientRect().width
+      slider.progress = slider.progress / slider.width * newWidth
+      slider.width = newWidth
+      slider.scale = newWidth / (slider.range[1] - slider.range[0])
     })
   }
 
@@ -135,7 +164,7 @@ class Slidery {
     if (!slider) return
     this.activeSliders[id] = {
       initialScreenPosition: event.screenX,
-      initialValue: slider.value
+      initialProgress: slider.progress
     }
     const handleMove = this.handleMouseMove(id)
     const handleUp = this.handleMouseUp(id)
@@ -158,7 +187,7 @@ class Slidery {
     this.activeSliders[id] = {
       initialScreenPosition: touch.screenX,
       touchId: touch.identifier,
-      initialValue: slider.value
+      initialProgress: slider.progress
     }
     const handleTouchMove = this.handleTouchMove(id)
     const handleTouchEnd = this.handleTouchEnd(id)
@@ -177,8 +206,8 @@ class Slidery {
     const activeSlider = this.activeSliders[id]
     if (!activeSlider) return
 
-    const { initialValue, initialScreenPosition } = activeSlider
-    const progress = initialValue + event.screenX - initialScreenPosition
+    const { initialProgress, initialScreenPosition } = activeSlider
+    const progress = initialProgress + event.screenX - initialScreenPosition
     this.adjustSlider(id, progress, true)
 
     this.activeSliders[id] = null
@@ -190,7 +219,6 @@ class Slidery {
     const slider = this.sliders[id]
     if (slider) {
       slider.nodes.liftNode.classList.remove('slidery-is-sliding')
-      slider.tempValue = null
       slider.targets.forEach(target => {
         target.node.classList.remove('slidery-is-sliding')
       })
@@ -200,13 +228,13 @@ class Slidery {
     const activeSlider = this.activeSliders[id]
     if (!activeSlider) return
 
-    const { initialValue, initialScreenPosition, touchId } = activeSlider
+    const { initialProgress, initialScreenPosition, touchId } = activeSlider
     const touch = [].slice
       .call(event.changedTouches)
       .find(t => t.identifier === touchId)
     if (!touch) return
 
-    const progress = initialValue + touch.screenX - initialScreenPosition
+    const progress = initialProgress + touch.screenX - initialScreenPosition
     this.adjustSlider(id, progress, true)
 
     this.activeSliders[id] = null
@@ -228,9 +256,9 @@ class Slidery {
     const activeSlider = this.activeSliders[id]
     if (!activeSlider) return
 
-    const { initialValue, initialScreenPosition } = activeSlider
+    const { initialProgress, initialScreenPosition } = activeSlider
     return event => {
-      const progress = initialValue + event.screenX - initialScreenPosition
+      const progress = initialProgress + event.screenX - initialScreenPosition
       this.adjustSlider(id, progress)
     }
   }
@@ -238,17 +266,19 @@ class Slidery {
     const activeSlider = this.activeSliders[id]
     if (!activeSlider) return
 
-    const { initialValue, initialScreenPosition, touchId } = activeSlider
+    const { initialProgress, initialScreenPosition, touchId } = activeSlider
     return event => {
       const touch = [].slice
         .call(event.changedTouches)
         .find(t => t.identifier === touchId)
       if (!touch) return
 
-      const progress = initialValue + touch.screenX - initialScreenPosition
+      const progress = initialProgress + touch.screenX - initialScreenPosition
       this.adjustSlider(id, progress)
     }
   }
+
+  // Helpers
   adjustTargets = (targets, newValue) => {
     targets.forEach(target => {
       if (target.node) {
@@ -268,20 +298,23 @@ class Slidery {
       }
     })
   }
-  adjustSlider = (id, value, setNewValue = false) => {
+  adjustSlider = (id, progress, setNewProgress = false) => {
     const slider = this.sliders[id]
     if (!slider) return
 
-    let newValue = value
-    if (newValue < 1) newValue = 1
-    if (newValue > slider.width) newValue = slider.width
+    let newProgress = progress
+    if (newProgress < 0) newProgress = 0
+    if (newProgress > slider.width) newProgress = slider.width
 
-    slider.nodes.liftNode.style.left = `${newValue}px`
-    slider.nodes.progressNode.style.width = `${newValue}px`
-    slider.tempValue = newValue
+    const newValue = slider.range[0] + newProgress / slider.scale
+    const newPosition = newProgress / slider.width * 100
 
-    if (setNewValue) {
-      slider.value = newValue
+    slider.nodes.liftNode.style.left = `${newPosition}%`
+    slider.nodes.progressNode.style.width = `${newPosition}%`
+    slider.value = newValue
+
+    if (setNewProgress) {
+      slider.progress = newProgress
     }
     this.adjustTargets(slider.targets, newValue)
   }
